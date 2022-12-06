a7fbf898f29adeb833f3ef7c368c7d377fabded8
AbdominalMS <abdelrahamm0335@gmail.com>
2022-12-06 10:58:20 +0200

    Changes

--- Equihealth/lib/main.dart
+++ Equihealth/lib/main.dart
@@ -1,74 +1,86 @@
+import 'package:equihealth/Components.dart';
+import 'package:equihealth/login.dart';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+      overlays: [SystemUiOverlay.bottom]);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Equihealth',
-      home: Scaffold(
-          backgroundColor: Colors.white,
-          body: Center(
-            child: Container(
-              width: 600,
-              height: 900,
-              alignment: Alignment.center,
-              child: Column(children: <Widget>[
-                const SizedBox(width: 50, height: 50),
-                Expanded(
-                  child:
-                      Image.asset('Assets/Logo.png', width: 200, height: 600),
-                ),
-                const SizedBox(width: 75, height: 75),
-                Text(
-                  'Choose language              اختر اللغة',
-                  style: TextStyle(
-                      color: HexColor("004aad"),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 14.1),
-                ),
-                const SizedBox(width: 200, height: 50),
-                Column(
-                  children: [
-                    Container(
-                      width: 317,
-                      height: 52,
-                      color: HexColor("004aad"),
-                      child: Center(
-                        child: Text(
-                          'English',
-                          style: TextStyle(
-                              color: HexColor("f8f8f8"),
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15),
-                        ),
-                      ),
+      home: Builder(
+          builder: (context) => Scaffold(
+              backgroundColor: Colors.white,
+              body: Center(
+                child: Container(
+                  width: 600,
+                  height: 900,
+                  alignment: Alignment.center,
+                  child: Column(children: [
+                    const SizedBox(width: 50, height: 50),
+                    Expanded(
+                      child: Image.asset('Assets/Logo.png',
+                          width: 200, height: 600),
                     ),
-                    const SizedBox(width: 200, height: 20),
-                    Container(
-                      width: 317,
-                      height: 52,
-                      color: HexColor("004aad"),
-                      child: Center(
-                          child: Text(
-                        'Arabic',
-                        style: TextStyle(
-                            color: HexColor("f8f8f8"),
-                            fontWeight: FontWeight.bold,
-                            fontSize: 15),
-                      )),
+                    const SizedBox(width: 75, height: 75),
+                    Text(
+                      'Choose language              اختر اللغة',
+                      style: TextStyle(
+                          color: HexColor("004aad"),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14.1),
                     ),
-                  ],
+                    const SizedBox(width: 200, height: 50),
+                    Column(
+                      children: [
+                        Container(
+                            width: 317,
+                            height: 52,
+                            color: HexColor("004aad"),
+                            child: Center(
+                                child: ElevatedButton(
+                              child: const Text('English'),
+                              onPressed: () {
+                                NavigateAndFinish(context, Login());
+                              },
+                              style: ElevatedButton.styleFrom(
+                                  minimumSize: const Size(317, 52),
+                                  primary: HexColor("004aad")),
+                            ))),
+                        const SizedBox(width: 200, height: 20),
+                        Container(
+                            width: 317,
+                            height: 52,
+                            color: HexColor("004aad"),
+                            child: Center(
+                                child: ElevatedButton(
+                              child: const Text('Arabic'),
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                  minimumSize: const Size(317, 52),
+                                  primary: HexColor("004aad")),
+                            ))),
+                      ],
+                    ),
+                    const SizedBox(
+                      width: 150,
+                      height: 150,
+                    ),
+                  ]),
                 ),
-                const SizedBox(
-                  width: 150,
-                  height: 150,
-                ),
-              ]),
-            ),
-          )),
+              ))),
     );
   }
 }
